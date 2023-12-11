2de57683eeae14033e336cb3654e28e720d866b8
Ilie2 <147348536+Ilie2@users.noreply.github.com>
2023-12-11 22:54:59 +0200

    Add files via upload

---
+++ components/header.php
@@ -0,0 +1,45 @@
+<header>
+    
+    <div class="header-1">
+
+        <a href="HOME.php" class="logo"><img src="images-0/logo.jpg" alt="" style="width:40px;height:50px;">Electro-PC Gaming</a>
+
+        <form action="" class="search-box-container">
+            <input type="search" id="search-box" placeholder="search here...">
+            <label for="search-box" class="fas fa-search"></label>
+        </form>
+
+    </div>
+
+    <div class="header-2">
+
+        <nav class="navbar">
+            <div class="dropdown">
+                <button class="dropbtn"><a href="#" class="fas fa-bars"> category
+                </button></a>
+                    <div class="dropdown-content">
+                        <a href="mice.php">mice</a>
+                        <a href="headsets.php">headsets</a>
+                        <a href="keyboards.php">keyboards</a>
+                        <a href="pcgaming.php">pc gaming</a>
+                        <a href="frigider.php">frigider</a>
+                        <a href="microunde.php">microunde</a>
+                        <a href="aragaz.php">aragaz</a>
+                    </div>
+            </div> 
+            <a href="HOME.php">home</a>
+            <a href="HOME.php #product">discounts</a>
+            <a href="feedback_1.php">FeedBack</a>
+            <a href="contacte.php">contact</a>
+            <a href="FAQ.php">FAQ</a>
+        </nav>
+
+        <div class="icons">
+            <a href="shoping cart.php" class="fas fa-shopping-cart"></a>
+            <a href="#" class="fas fa-heart"></a>
+            <a href="#" class="fas fa-user-circle"></a>
+        </div>
+
+    </div>
+
+</header>
